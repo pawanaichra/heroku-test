d7649877ac8d65235a5d3f15329f4a87da904607
Pawan Aichra <pawanaichra@iitkgp.ac.in>
2020-04-21 22:17:58 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import base64
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+  try:
+    print("hello")
+  except Exception as err:
+    print(err)
+  return "done"
