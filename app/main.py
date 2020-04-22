523e7e3361979c7680f659394af1489667c5f96d
Pawan Aichra <pawanaichra@iitkgp.ac.in>
2020-04-22 17:54:04 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import base64
 from flask import Flask
 from flask import request
+from app.logistic import logistic_func
   
 app = Flask(__name__) 
   
@@ -9,9 +10,9 @@ def root():
   try:
     imgstring = request.form['image']
     imgdata = base64.b64decode(imgstring)
-    
-    with open("5.jpg", 'wb') as f:
-      f.write(imgdata)
-    return ("hello")
+    result = logistic_func(imgdata)
+#     with open("img.jpg", 'wb') as f:
+#       f.write(imgdata)
+    return result
   except Exception as err:
     print(err)
