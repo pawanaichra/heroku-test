70155184989df0354298c5f56517e427e13e25f4
Pawan Aichra <pawanaichra@iitkgp.ac.in>
2020-04-22 18:33:45 +0530

    Update logistic.py

--- app/logistic.py
+++ app/logistic.py
@@ -1,3 +1,18 @@
-def logistic_func(imgdata):
-  print(imgdata)
-  return "Alluvial"
+import matplotlib.pyplot as plt
+import numpy as np
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+def logistic_func():
+  l=plt.imread("img.jpg").transpose(2,0,1)
+  r=l[0,:,:].mean()
+  g= l[1,:,:].mean()
+  b = l[2,:,:].mean()
+  theta = np.array([-2.17299476, -0.06251461, -0.28642574,  0.28075228])
+  X=np.array([1,r,g,b])
+  z = np.dot(X, theta)
+  prob = sigmoid(z)
+  print(prob)
+  if(prob>.5):
+    return("Laterite")
+  else:
+    return("Alluvial")
