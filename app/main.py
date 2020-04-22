f720e7881f10d7b1e198025fe6ca6acdba1340ac
Pawan Aichra <pawanaichra@iitkgp.ac.in>
2020-04-22 18:09:56 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import base64
 from flask import Flask
 from flask import request
+import numpy as np
+from PIL import Image
+import cv2
 from app.logistic import logistic_func
   
 app = Flask(__name__) 
@@ -10,7 +13,9 @@ def root():
   try:
     imgstring = request.form['image']
     imgdata = base64.b64decode(imgstring)
-    result = logistic_func(imgdata)
+    image = Image.open(io.BytesIO(imgdata))
+    rgb = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    result = logistic_func(rgb)
 #     with open("img.jpg", 'wb') as f:
 #       f.write(imgdata)
     return result
