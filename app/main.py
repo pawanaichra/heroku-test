f5be7b870b537ae3fea21454f157d958e73926bc
Pawan Aichra <pawanaichra@iitkgp.ac.in>
2020-04-22 17:48:38 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
-from flask import Flask 
+import base64
+from flask import Flask
+from flask import request
   
 app = Flask(__name__) 
   
-@app.route("/") 
-def root(): 
-        return "test"
+@app.route("/", methods=['POST'])
+def root():
+  try:
+    imgstring = request.form['image']
+    imgdata = base64.b64decode(imgstring)
+    
+    with open("5.jpg", 'wb') as f:
+      f.write(imgdata)
+    return ("hello")
+  except Exception as err:
+    print(err)
